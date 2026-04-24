74220b4fce80492e6768c8d63b023eb2b756c590
amq-tutti <tutti-amq@proton.me>
2026-04-24 18:13:31 +0300

    Update title and description in config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const config = {
     localStoragePrefix: "vaundy-pr",
-    title: "Party Rank Sorter",
-    description: "Party rank sorter for your custom list of songs."
+    title: "Vaundy",
+    description: "Party rank sorter for Vaundy anime songs."
 };
